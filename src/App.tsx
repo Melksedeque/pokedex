852a267168c86f930fa563640b0fb5bbb19415e6
Melksedeque <freelancer@melksedeque.com.br>
2025-04-27 15:06:34 -0300

    Componentização do App para Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import pokemonLogo from "./assets/images/International_Pokémon_logo.svg";
-import pokeball from "./assets/images/Pokéball.svg";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="/" title="Pokédex">
-          <img src={pokemonLogo} className="logo" alt="Pokémon logo" />
-        </a>
-      </div>
-      <h1>
-        <img src={pokeball} alt="Pokéball" />
-        Pokédex
-      </h1>
-    </>
-  );
+  return <Header />;
 }
 
 export default App;
