58f815cab1c300bc9bc97bca2a40853e6aa65126
Melksedeque <freelancer@melksedeque.com.br>
2025-04-28 15:23:17 -0300

    Arquivos de configuração do Jest

---
+++ jest.config.mjs
@@ -0,0 +1,17 @@
+export default {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+};
