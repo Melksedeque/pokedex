5c921e2da3b6f7dbb7153025046d50d7e7db829f
Melksedeque <freelancer@melksedeque.com.br>
2025-06-06 16:41:37 -0300

    Criação de lista inicial de pokemon (em desenvolvimento)

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,7 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ListaPokedex() {
   // Componente para listar os pokemons buscando na API
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://pokeapi.co/v2/pokemon?offset=20&limit=20")
+      .then((res) => res.json())
+      .then((res) => {
+        setPokemon(res.results);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <div>Carregando...</div>
+      ) : (
+        <ul>
+          {pokemon.map((pokemon, index) => (
+            <li key={index}>{pokemon}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
