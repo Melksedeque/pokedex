a1cb048f90ae78fd18a329da3fabea6fe50e5feb
Melksedeque <freelancer@melksedeque.com.br>
2025-06-06 16:26:31 -0300

    useState de pokemon e loading

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+
 export default function ListaPokedex() {
   // Componente para listar os pokemons buscando na API
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(false);
 }
