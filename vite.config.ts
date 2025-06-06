da86c9da2e30e7eed76c6282835494e64533b184
Melksedeque <freelancer@melksedeque.com.br>
2025-06-06 16:45:31 -0300

    Adição de definições de aliases com seus respectivos path.resolves

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,13 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      components: path.resolve(__dirname, "./src/components"),
+      data: path.resolve(__dirname, "./src/data"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      store: path.resolve(__dirname, "./src/store"),
+      styles: path.resolve(__dirname, "./src/styles"),
+      types: path.resolve(__dirname, "./src/types"),
     },
   },
 });
