979c75515cd225ba01ddd150fecdbd5617f8b2cf
Melksedeque <freelancer@melksedeque.com.br>
2025-09-28 14:33:11 -0300

    Adição de filtragem e busca no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,47 @@
 import pokeball from "assets/images/Pokéball.svg";
 import styles from "./Header.module.scss";
-import Formulario from "./Formulario";
+import PokemonFilters, { FilterState } from "../PokemonFilters";
+import { SortOption } from "../../types/pokemon";
 
-export default function Header() {
+interface HeaderProps {
+  onFilterChange: (filters: FilterState) => void;
+  onSortChange: (sort: SortOption) => void;
+  onSearchChange: (search: string) => void;
+  currentFilters: FilterState;
+  currentSort: SortOption;
+  currentSearch: string;
+  isLoading?: boolean;
+}
+
+export default function Header({
+  onFilterChange,
+  onSortChange,
+  onSearchChange,
+  currentFilters,
+  currentSort,
+  currentSearch,
+  isLoading
+}: HeaderProps) {
   return (
     <header className={styles.appHeader}>
-      <h1 className={styles.appTitle}>
-        <img src={pokeball} alt="Pokéball" />
-        Pokédex
-      </h1>
-      <Formulario />
+      <div className={styles.headerContent}>
+        <h1 className={styles.appTitle}>
+          <img src={pokeball} alt="Pokéball" />
+          Pokédex
+        </h1>
+        
+        <div className={styles.filtersSection}>
+          <PokemonFilters
+            onFilterChange={onFilterChange}
+            onSortChange={onSortChange}
+            onSearchChange={onSearchChange}
+            currentFilters={currentFilters}
+            currentSort={currentSort}
+            currentSearch={currentSearch}
+            isLoading={isLoading}
+          />
+        </div>
+      </div>
     </header>
   );
 }
